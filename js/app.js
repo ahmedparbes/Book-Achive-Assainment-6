819ef561297fb5e21fd6dcf3292fd14b5ad9d4cc
ahmedparbes <fbhelpsbd@gmail.com>
2021-09-02 03:16:42 +0600

    added

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const inputField = document.getElementById('input-field')
 
 searchButton.addEventListener('click', function () {
     const search = inputField.value;
+    inputField.value = '';
     const url = `http://openlibrary.org/search.json?q=${search}`
     fetch(url)
         .then(res => res.json())
@@ -31,33 +32,5 @@ const displayResult = books => {
         </div>
 `;
         displayDetails.appendChild(div);
-
-
-
-
-
-
-
-
-        /* const div = document.createElement('div')
-        div.classList.add('col')
-        div.innerHTML = `
-        <div class="card h-100">
-            <img src="${url}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${book.title}</h5>
-                <p class="card-text">${book.author_name}</p>
-            </div>
-        </div>
-`;
-        displayDetails.appendChild(div) */
     })
 }
-
-/*  <div class="card">
-        <img src="${url}" class="card-img-top" alt="...">
-    <div class="card-body">
-    <h5 class="card-title">${book.author_name}</h5>
-    <p class="card-text">${book.title}</p>
-    </div> */
-    // </div> 
