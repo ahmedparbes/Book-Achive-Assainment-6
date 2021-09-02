e7eaca02c5fc003c51bf9b7b2390a8e15b79331c
ahmedparbes <fbhelpsbd@gmail.com>
2021-09-02 09:55:46 +0600

    add

--- js/app.js
+++ js/app.js
@@ -1,36 +1,48 @@
-const searchButton = document.getElementById('search-button')
-const inputField = document.getElementById('input-field')
-
-searchButton.addEventListener('click', function () {
-    const search = inputField.value;
-    inputField.value = '';
-    const url = `http://openlibrary.org/search.json?q=${search}`
+const searchBox = () => {
+    const searchField = document.getElementById('input-field');
+    const value = searchField.value;
+    searchField.value = '';
+    const url = `http://openlibrary.org/search.json?q=${value}`
     fetch(url)
         .then(res => res.json())
-        .then(data => displayResult(data.docs))
-})
+        .then(data => loadBooks(data.docs))
+}
 
-const displayResult = books => {
+const loadBooks = books => {
 
-    // console.log(books);
-    const displayDetails = document.getElementById('meal-details')
-    books.forEach(book => {
-        // const url = https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg
-        const url = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+    //result found
+    let len = books.length;
+    console.log('Result Found', len);
+
+
+    for (let i = 0; i <= 10; i++) {
+        const displayDetails = document.getElementById('details');
+        const result = books[i];
+        console.log(result);
+        const url = `https://covers.openlibrary.org/b/id/${result.cover_i}-M.jpg`
         fetch(url)
-            .then(res => res.json())
-            .then(data => (data))
+            .then(res => res.json)
+            .then(data => console.log(data))
+
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-<div class="card h-100">
+            
+            <div class="card h-100">
             <img src="${url}" class="card-img-top" alt="...">
             <div class="card-body">
-               Book Name : <h5 class="card-title">${book.title}</h5>
-              Book Author:  <p class="card-text">${book.author_name}</p>
+               Book Name : <h5 class="card-title">${result.title}</h5>
+               <hr>
+               Book Author:  <h6 class="card-text">${result.author_name}</h6>
+              <hr>
+               Book Publisher:  <h6 class="card-text">${result.publisher}</h6>
+              <hr>
+              First Published <h6>${result.first_publish_year}</h6>
+              <hr>
             </div>
         </div>
-`;
+            `;
         displayDetails.appendChild(div);
-    })
+
+    }
 }
